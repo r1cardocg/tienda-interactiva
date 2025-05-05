bb1b33303e29dbae83bfbdf4145f0a4ff863f013
unknown <ricalderong@unal.edu.co>
2025-05-05 15:47:36 -0500

    changres

--- App.js
+++ App.js
@@ -1,4 +1,28 @@
-function init(){
-    var productos = document.getElementById("productos").value;
-    productos = productos.innerHTML== "Hola";
+function init () {
+    var productos = document.getElementById("productos");
+    productos = productos.innerHTML = 
+        `
+        <div>
+        <label>
+        Producto 1
+        </label>
+        </div>
+        <input type="number" name="" id="cantidad" min="0" max="20"> 
+        <button type="button" class="btn btn-primary" onclick="carrito()">Agregar</button>
+        `; 
+}
+
+function carrito () {
+    let carrito = document.getElementById ("carrito");
+    let cantidad = document.getElementById ("cantidad").value;
+    carrito.innerHTML = 
+    `
+    <div> 
+        <h6>
+        Cantidad: ${cantidad}
+        </h6>
+    </div>
+    `
+    alert ("Se agrego correctamente al carrito")
+
 }
